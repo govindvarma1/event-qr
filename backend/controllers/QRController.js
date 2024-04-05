afda67545683cb82c64aa79917676436f9dfabcf
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-04-05 09:45:52 +0530

    completed google services

--- backend/controllers/QRController.js
+++ backend/controllers/QRController.js
@@ -1,8 +1,11 @@
-import { getAuth, getSpreadSheet } from "../services/GoogleSheetServices.js";
+import { getAuth, getSpreadSheet, getSpreadSheetValues, updateSpreadSheetsValues } from "../services/GoogleSheetServices.js";
 
 export const ScanQR = async (req, res, next) => {
     try {
-        res.send("ScanQR");
+        const auth =await getAuth();
+        const spreadsheetId = "1N3lo_mcPvFsW8vry0CZdP0rEvfhW8UGaVjQK-AJzUpo";
+        const sheets = await updateSpreadSheetsValues({spreadsheetId, auth, range: "Sheet1!A2:C4", data: [["1", "2", "3"], ["1", "2", "3"], ["1", "2", "3"]]});
+        res.send(sheets);
     } catch(ex) {
         next(ex);
     }
--- backend/services/GoogleSheetServices.js
+++ backend/services/GoogleSheetServices.js
@@ -1,5 +1,5 @@
 import { google } from "googleapis";
-const sheets = google.sheets("v4");
+const sheets = google.sheets('v4');
 
 const getAuth = async() => {
     const auth = new google.auth.GoogleAuth({
@@ -10,16 +10,34 @@ const getAuth = async() => {
     return client;
 }
 
-const getSpreadSheet = async({spreadSheetId, auth}) => {
-    const spreadsheets = await sheets.spreadsheets.get({
-        spreadSheetId,
+const getSpreadSheet = async ({ spreadsheetId, auth }) => {
+    const spreadsheet = await sheets.spreadsheets.get({
+        spreadsheetId,
         auth
+    });
+    return spreadsheet;
+}
+
+const getSpreadSheetValues = async({spreadsheetId, auth, range}) => {
+    const spreadsheet = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        auth,
+        range
     })
-    return spreadsheets;
+    return spreadsheet.data;
 }
 
-const getSpreadSheetValues = async({spredSheetId, auth, sheetName}) => {
+const updateSpreadSheetsValues = async ({ spreadsheetId, auth, range, data }) => {
+    const res = await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        auth,
+        range,
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: data
+        }
+    });
+    return res;
+};
 
-}
-
-export {getAuth, getSpreadSheet, getSpreadSheetValues};
+export {getAuth, getSpreadSheet, getSpreadSheetValues, updateSpreadSheetsValues};
