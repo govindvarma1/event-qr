53a607b1c89e12e37d4f73d25c88dfc25eb59c84
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-04-20 11:47:11 +0530

    removed app

--- app/firebase.js
+++
@@ -1,6 +0,0 @@
-import firebaseConfig from "./firebase.config";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
